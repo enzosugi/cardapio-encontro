ba26ff9585cf0368e41a9d90e6944b629ba5a136
enzosugi <enzo.sugi@gmail.com>
2021-12-14 14:57:47 -0300

    descritivo para os pratos

--- src/components/corpo/Corpo.jsx
+++ src/components/corpo/Corpo.jsx
@@ -8,30 +8,44 @@ export default function Corpo() {
                 <h3>PRATOS EXECUTIVOS</h3>
                 <ul>
                     <li>Carne de panela <span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Acompanha arroz, feijão, farofa mais salada ou fritas</span>
                     <li>Carne seca<span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Acompanha arroz, feijão, farofa mais salada ou fritas</span>
                     <li>Carne de churrasco<span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Acompanha arroz, feijão, farofa mais salada ou fritas</span>
                     <li>Picanha e fritas <span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Acompanha arroz, feijão, farofa mais salada ou fritas</span>
                     <li>Picadinho de carne <span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Acompanha arroz, feijão, farofa mais salada ou fritas</span>
                     <li>Strogonoff de Frango<span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Acompanha arroz, feijão, farofa mais salada ou fritas</span>
                     <li>Sopa de carne <span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Acompanha arroz, feijão, farofa mais salada ou fritas</span>
                 </ul>
             </div>
 
             <div className="lanches">
                 <h3>LANCHES</h3>
                 <ul>
-                    <li>Carne de panela <span className="preco">...R$25,00</span></li>
-                    <li>Carne seca<span className="preco">...R$25,00</span></li>
-                    <li>Carne de churrasco<span className="preco">...R$25,00</span></li>
-                    <li>Picanha e fritas <span className="preco">...R$25,00</span></li>
-                    <li>Picadinho de carne <span className="preco">...R$25,00</span></li>
-                    <li>Strogonoff de Frango<span className="preco">...R$25,00</span></li>
-                    <li>Sopa de carne <span className="preco">...R$25,00</span></li>
+                    <li>X-Burguer <span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Hamburguer 120g, mussarela, maionesse no pão tradicional</span>
+                    <li>X-Salada<span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Hamburguer 120g, mussarela, maionesse no pão tradicional</span>
+                    <li>X-Bacon<span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Hamburguer 120g, mussarela, maionesse no pão tradicional</span>
+                    <li>X-Egg <span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Hamburguer 120g, mussarela, maionesse no pão tradicional</span>
+                    <li>X-Churrasco <span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Hamburguer 120g, mussarela, maionesse no pão tradicional</span>
+                    <li>X-Frango<span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Hamburguer 120g, mussarela, maionesse no pão tradicional</span>
+                    <li>X-Tudo<span className="preco">...R$25,00</span></li>
+                    <span className="descricao">Hamburguer 120g, mussarela, maionesse no pão tradicional</span>
                 </ul>
             </div>
 
             <div className="salgados">
-                <h3>SALGADOS</h3>
+                <h3>MATINAIS</h3>
                 <ul>
                     <li>Carne de panela <span className="preco">...R$25,00</span></li>
                     <li>Carne seca<span className="preco">...R$25,00</span></li>
